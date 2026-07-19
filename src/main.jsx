2e3b611d088b563a4287f499f4d2ba9bdd51ad45
priscillazaehringer <priscilla@priscillazaehringer.com>
2026-07-19 18:15:57 -0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IntakeWizard from "./IntakeWizard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <IntakeWizard />
+  </React.StrictMode>
+);
